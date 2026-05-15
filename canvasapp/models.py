ba7ccc0bc6eb53d14096f627d00dbf9736cd1214
kimanibryan04-del <kimanibryan04@gmail.com>
2026-05-15 16:49:48 +0300

    new commit

--- canvasapp/models.py
+++ canvasapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='properties/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        # This is important for the Sitemap to work!
+        from django.urls import reverse
+        return reverse('property_detail', args=[str(self.id)])
